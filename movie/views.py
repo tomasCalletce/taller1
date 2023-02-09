e1dc38171781a74dfd5ae797e6795703cd17a18b
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-02-09 11:19:34 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html',{'name':'Tomas Calle Espinal'})
+    return render(request,'home.html',{'name':' Tomas Calle Espinal '})
