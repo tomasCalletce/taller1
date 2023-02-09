2ea3d458266d57dc591e5bcdaaa8e74c4429cd3a
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-02-09 11:36:18 -0500

    about page

--- movie/views.py
+++ movie/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html',{'name':' Tomas Calle Espinal '})
+
+def about(request):
+    return render(request,'about.html')
+
